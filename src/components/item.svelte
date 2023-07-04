8f1b5655476a922ebf7158f787dc637326d94695
AddisonRogers <addisonrogers@protonmail.com>
2023-07-04 19:57:57 +0100

    more changes

--- src/components/item.svelte
+++ src/components/item.svelte
@@ -1,1 +1,28 @@
-<!-- This is basically the posts you see in /posts and  -->
+<!-- This is the posts you see in /posts and  -->
+
+<script lang="ts">
+    export let authour : string = "John Doe";
+    export let title : string = "Funky title";
+    export let server : string = "Funky server";
+    export let community : string = "Funky community";
+    export let image : MediaImage; 
+
+    export let Date : Date;
+    export let CommentCount : number = 0;
+    export let Original : URL;
+    export let Likes : number = 0;
+    
+    export let Style : string = "Original"; 
+    /* 
+    Original, Old
+    */
+</script>
+
+<!-- This needs to have two different ig styles depednent on if a property is true or not-->
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<img src=image loading="lazy"/>
+
+<style>
+
+</style>
