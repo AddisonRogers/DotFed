7a4669b2a7c680ad6f57ea829763c7fd3e732200
AddisonRogers <AddisonRogers@protonmail.com>
2023-07-17 23:13:23 +0100

    removing wasm

--- src/wasm_modules/src/lemmy.zig
+++
@@ -1,8 +0,0 @@
-// Acts as a interface for the http client module
-// This needs to take in 3 different type of calls
-// Specific feed: One feed
-
-// https://lemmy.ml/api/v3/
-// https://lemmy.ml/api/v3/community/list?sort=Hot
-// https://lemmy.ml/api/v3/community/asklemmy (not sure)
-// https://lemmy.ml/api/v3/feeds/c/asklemmy
