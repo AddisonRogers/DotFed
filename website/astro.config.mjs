1d41f3778af079b3e15c15f215e48864b11d0f11
Addison <addisonrogers@protonmail.com>
2023-07-27 15:13:43 +0100

    astro template

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import purgecss from "astro-purgecss";
-
-import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable Svelte to support Svelte components.
-  integrations: [svelte(), tailwind(), sitemap(), purgecss()],
-  output: "server",
-  adapter: deno({})
+	// Enable Svelte to support Svelte components.
+	integrations: [svelte()],
 });
