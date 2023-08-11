5f78e6bfec29ea5c28b7d965eb447ac458e584cc
dotracc <addisonrogers@protonmail.com>
2023-08-11 19:48:56 +0100

    h h h h

--- DotFed/Routing.cs
+++ DotFed/Routing.cs
@@ -14,6 +14,11 @@ public static class Routing
     public static void AddRoutes(this WebApplication app, Db? db)
     {
         app.MapGet("/", () => Results.Extensions.RazorSlice("/Slices/Index.cshtml", "NAME"));
+        
+        
+        
+        
+        
         app.MapPost("/", () => Content("<span>Hello World</span>", "text/html"));
         //https://htmx.org/examples/infinite-scroll/
         
--- Old/Program.cs
+++
@@ -1,7 +0,0 @@
-using DotFedWorker;
-
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
-    .Build();
-
-host.Run();
