a272d52f48e6eaf3abb59047069a20d5a0811af5
AddisonRogers <AddisonRogers@protonmail.com>
2023-08-20 12:30:35 +0100

    Implement pagination for main page
    Changed layout in index.cshtml to support pagination, removing static postTemplate code with a loop that generates divs for each page. Included actions to increment page count and trigger page reload when the last page divider is revealed.
    
    Adjusted the routing in index.cshtml.cs, supporting page number increments and rendering different MarkUp depending if request comes from Htmx. This is to improve user experience by enabling page-based navigation.

--- DotFed/Slices/index.cshtml.cs
+++ DotFed/Slices/index.cshtml.cs
@@ -1,13 +1,17 @@
+using Htmx;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DotFed.Slices;
 
 public class IndexModel
 {
+    public static int count = 0;
     public static void Routes(RouteGroupBuilder app)
     {
         app.MapGet("/", () => Results.Extensions.RazorSlice("/Slices/Index.cshtml"));
-        
+        app.MapPost("/", (int? page, HttpRequest request) => request.IsHtmx()
+            ? Results.Extensions.RazorSlice("/Slices/_post.cshtml", page)
+            : Results.Extensions.RazorSlice("/Slices/Index.cshtml"));
     }
     
     
