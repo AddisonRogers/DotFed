c8090fa1ec8b7a261faadb135ef40e89a363748b
Addison <addisonrogers@protonmail.com>
2023-08-20 05:41:15 +0100

    fixes

--- DotFed/Program.cs
+++ DotFed/Program.cs
@@ -5,8 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 //var db = new Db(new DbContextOptionsBuilder<Db>().UseInMemoryDatabase("DotFed").Options);
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.AddRoutes();
 app.Run();
--- DotFed/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./Slices/*.cshtml"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
