40539e472cf6b7e2bcfe078a2766d89a6e6b23c5
Addison <addisonrogers@protonmail.com>
2023-08-20 05:24:34 +0100

    removing tailwind and replacing it with twind as it doesn't work otherwise woopsie

--- DotFed/Program.cs
+++ DotFed/Program.cs
@@ -5,5 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 //var db = new Db(new DbContextOptionsBuilder<Db>().UseInMemoryDatabase("DotFed").Options);
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.AddRoutes();
 app.Run();
