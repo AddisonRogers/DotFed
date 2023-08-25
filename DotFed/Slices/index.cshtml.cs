170192b3902b2a72fe7a53763c367d0760cdbf70
Addison <addisonrogers@protonmail.com>
2023-08-25 17:07:41 +0100

    h

--- DotFed/Slices/index.cshtml.cs
+++ DotFed/Slices/index.cshtml.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using Htmx;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Primitives;
+using static Microsoft.AspNetCore.Http.Results;
 
 namespace DotFed.Slices;
 
@@ -10,11 +12,31 @@ public class IndexModel
     {
         app.MapGet("/", () => Results.Extensions.RazorSlice("/Slices/Index.cshtml"));
         app.MapPost("/", (int? page, HttpRequest request) => request.IsHtmx()
-            ? Results.Extensions.RazorSlice("/Slices/_post.cshtml", page)
+            ? MorePosts(page ?? 1)
             : Results.Extensions.RazorSlice("/Slices/Index.cshtml"));
     }
-    
-    
+
+    private static IResult MorePosts(int page)
+    {
+        var html = new StringBuilder();
+        html.Append("<div hx-get='/'>Page: " + page + "</div>");
+        for (var i = 1; i <= 10; i++)
+        {
+            if (i == 10)
+            {
+                count++;
+                html.Append(
+                    $"""<div hx-post="/?page={count}" hx-trigger="revealed" hx-swap="afterend">Post {i}</div>""");
+
+            }
+            else
+            {
+                html.Append($"<div>Post {i}</div>");
+            }
+        }
+
+        return Content(html.ToString(), "text/html");
+    }
     
 }
 
