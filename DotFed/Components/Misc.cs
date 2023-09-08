b833ec9c81d28beff8119d3ce54b1cd29d6d00fa
Addison Rogers <addisonrogers@protonmail.com>
2023-09-08 15:58:43 +0100

    Resolve merge conflicts and streamline codebase
    Conflicts generated during merging were resolved for clean compilation. The InMemory dependency was preserved over PostgreSQL as per project requirements, reflected in DotFed.csproj. A redundant database-related commented line was removed from Program.cs.
    
    For increased clarity, console output in Misc.cs was simplified and the function NewPage was modified to return a ContentHttpResult rather than a static string, this would allow for more complex dynamic content in the future.
    
    Superfluous whitespace and commented code in Routing.cs were removed for clean reading. An unresolved merge issue between two versions was rectified, keeping the project-specific necessary code.
    
    Minor script organization changes were made in the HTML output section of Misc.cs to prevent duplication and misplacement of scripts.

--- DotFed/Components/Misc.cs
+++ DotFed/Components/Misc.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 public static class Misc
 {
     public static string HelloWorld() {
 
-        System.Console.WriteLine("ooh wee I can do code and get props");
+        //System.Console.WriteLine("ooh wee I can do code and get props");
 
 
         return ("<h1>Hello World mf</h1>");
@@ -12,15 +13,17 @@ public static class Misc
 
     public static string HelloBlank(string props) {
 
-        System.Console.WriteLine($"ooh wee I can do code and get {props}");
+        System.Console.WriteLine($"{props}");
 
 
         return ($"<h1>Hello {props} mf</h1>");
     }
+    
+    
 
-    public static string NewPage(string body) {
+    public static ContentHttpResult NewPage(string body) {
 
-        return (
+        return TypedResults.Content(
             $"""
             <!DOCTYPE html>
             <html>
@@ -29,10 +32,10 @@ public static class Misc
                     <title>DotFed</title>
                     <script src="https://unpkg.com/htmx.org@1.9.4" integrity="sha384-zUfuhFKKZCbHTY6aRR46gxiqszMk5tcHjsVFxnUo8VMus4kHGVdIYVbOYYNlKmHV" crossorigin="anonymous"></script>
                     <script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/@unocss/runtime/core.global.js"></script>        
                     <meta name="viewport" content="width=device-width, initial-scale=1">
                     <meta charset="UTF-8">
-                    <script src="https://kit.fontawesome.com/4047d6e26b.js" crossorigin="anonymous"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/@@unocss/runtime"></script>
+                    
 
                 </head>
 
@@ -40,7 +43,7 @@ public static class Misc
                 {body}
                 </body>
             </html>
-            """
+            """, "text/html"
             );
     }
 
